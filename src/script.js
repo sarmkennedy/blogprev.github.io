a0545db24c89de4607746ebccb276f9599d533e9
Sarah Kennedy <sarmken@gmail.com>
2026-06-05 15:13:54 -0700

    sets up timeframe changes with javascript

--- src/script.js
+++ src/script.js
@@ -1,12 +1,35 @@
 const hoursElements = document.querySelectorAll('.hours');
 const timeframeButtons = document.querySelectorAll('.timeframes button');
-console.log(hoursElements);
-console.log("Found " + hoursElements.length + " hour elements");
-console.log("Found " + timeframeButtons.length + " timeframe buttons");
+const previousLabels = document.querySelectorAll('.previous');
+const prevHoursElements = document.querySelectorAll('.prev-hours');
 
-timeframeButtons.forEach(button => {
+let activityData = [];
+
+function updateCards(timeframe) {
+    activityData.forEach((activity,index) => {
+        const currentValue = activity.timeframes[timeframe].current;
+        const previousValue = activity.timeframes[timeframe].previous;
+
+        hoursElements[index].textContent = currentValue;
+        prevHoursElements[index].textContent = previousValue;
+    });
+}
+
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        activityData = data;
+        console.log("Data loaded!");
+        console.log(activityData);
+        updateCards('weekly'); 
+    })
+    .catch(error => {
+        console.error("Error loading data:", error);
+    });
+
+    timeframeButtons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log("A button was clicked!");
-        console.log("The button text was: " + button.textContent);
+        const timeframe = button.textContent.toLowerCase();
+        updateCards(timeframe);
     });
 });
